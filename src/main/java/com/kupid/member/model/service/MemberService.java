d906ed6808c6cb99a8170485c94a7b427d985f99
JinHyung-Park <trupjh@gmail.com>
2024-05-19 19:12:11 +0900

    #1 group, member Dao, Dto, Service 작성

--- src/main/java/com/kupid/member/model/service/MemberService.java
+++ src/main/java/com/kupid/member/model/service/MemberService.java
@@ -1,8 +1,12 @@
 package com.kupid.member.model.service;
 
-import static com.kupid.common.JDBCTemplate.*;
+import static com.kupid.common.JDBCTemplate.close;
+import static com.kupid.common.JDBCTemplate.commit;
+import static com.kupid.common.JDBCTemplate.getConnection;
+import static com.kupid.common.JDBCTemplate.rollback;
 
 import java.sql.Connection;
+import java.util.List;
 
 import com.kupid.member.model.dao.MemberDao;
 import com.kupid.member.model.dto.MemberDto;
@@ -15,9 +19,17 @@ public class MemberService {
 		close(conn);
 		return m;
 	}
+	public List<Object> selectMemberForProfile(String id) {
+		Connection conn = getConnection();
+		List<Object> result = dao.selectMemberForProfile(conn, id);
+		close(conn);
+		return result;
+	}
 	public int checkNickname(String nickname) {
 		Connection conn = getConnection();
 		int result = dao.checkNickname(conn, nickname);
+		if(result>0) commit(conn);
+		else rollback(conn);
 		close(conn);
 		return result;
 	}
